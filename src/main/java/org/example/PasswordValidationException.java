71959efa0c8db2b5b9fa58920c4f34a951caca54
Oil Wongmayura <152672793+BenjapornW@users.noreply.github.com>
2024-04-21 15:02:11 +1000

    Add files via upload

---
+++ src/main/java/org/example/PasswordValidationException.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class PasswordValidationException extends Exception{
+
+    public PasswordValidationException (String message, Throwable cause)
+    {
+        super (message, cause);
+    }
+
+}
